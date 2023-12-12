89feadb804adef987fd1dcb881dba563402eec06
Vitor Soares <vitorsoaresf@gmail.com>
2023-12-12 19:45:43 -0300

    feat: implements function average years old

--- Array_Collections_C-CodigoInicial/bytebank_ATENDIMENTO/Program.cs
+++ Array_Collections_C-CodigoInicial/bytebank_ATENDIMENTO/Program.cs
@@ -1,2 +1,17 @@
 ﻿Console.WriteLine("Boas Vindas ao ByteBank, Atendimento.");
 
+void TestaArrayInt()
+{
+    int[] idades = new int[5] { 30, 30, 10, 12, 13};
+
+    Console.WriteLine( $"Tamanho do arrray: {idades.Length}");
+
+    foreach(int idade in idades)
+    {
+        Console.WriteLine($"idade: {idade}");
+    }
+
+    Console.WriteLine($"A médias das idade é: {idades.Average()}");
+}
+
+TestaArrayInt();
