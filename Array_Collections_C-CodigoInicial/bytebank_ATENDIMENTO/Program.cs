3293d64e98fd93beaed69ec710f073aa676ca3f3
Vitor Soares <vitorsoaresf@gmail.com>
2023-12-12 20:11:13 -0300

    feat: implements calc mediana

--- Array_Collections_C-CodigoInicial/bytebank_ATENDIMENTO/Program.cs
+++ Array_Collections_C-CodigoInicial/bytebank_ATENDIMENTO/Program.cs
@@ -31,5 +31,29 @@ void TestaBuscarPalavra()
     }
 }
 
-// TestaArrayInt();
-TestaBuscarPalavra();
+void TestaMediana(Array array)
+{
+    if((array == null) || (array.Length == 0))
+    {
+        Console.WriteLine("Array para calculo da mediana está vazio ou nulo");
+        return;
+    }
+
+    double[] numerosOrdenados = (double[])array.Clone();
+    Array.Sort(numerosOrdenados);
+
+    int tamanho = numerosOrdenados.Length;
+    int meio = tamanho / 2;
+    double mediana = (tamanho % 2 != 0) ? numerosOrdenados[meio] : (numerosOrdenados[meio] + numerosOrdenados[meio -1]) / 2;
+
+    Console.WriteLine($"A mediana é = {mediana}");
+}
+
+Array amostra = Array.CreateInstance(typeof(double), 5);
+amostra.SetValue(5.9, 0);
+amostra.SetValue(1.8, 1);
+amostra.SetValue(7.1, 2);
+amostra.SetValue(10, 3);
+amostra.SetValue(6.9, 4);
+
+TestaMediana(amostra);
